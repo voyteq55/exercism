4930d573153007f5e97b70058c29d87686d68160
voyteq55 <53490891+voyteq55@users.noreply.github.com>
2023-10-07 13:25:35 +0200

    Solve Leap (OCaml) #2

--- ocaml/leap/leap.ml
+++ ocaml/leap/leap.ml
@@ -1,2 +1,6 @@
-let leap_year year = 
-    year mod 400 = 0 || (year mod 100 != 0 && year mod 4 = 0)
+let leap_year = function
+    | year when year mod 400 = 0 -> true
+    | year when year mod 100 = 0 -> false
+    | year -> year mod 4 = 0
+    
+   
